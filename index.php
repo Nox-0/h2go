15d281df66e2b980c53b00bc1e7f427e9a48ed6e
Nox-0 <dbublik11@gmail.com>
2018-05-04 14:42:46 +1200

    CSS, homepage and planning doc
    This is the css file that controls the css, along with the
    index/homepage and edits made to the planning doc.

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<title>Home </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
+</head>
+<body>
+	<div id="nav-bar-container">
+		<div id="nav-bar">
+			<a href="index.php"><img src="images/logo.png"  id="logo-image"/></a>
+			<nav>
+				<ul id="nav-buttons">
+					<li class="nav-button"><a href="shop.php">SHOP</a></li>
+					<li class="nav-button"><a href="signup.php">SIGN UP</a></li>
+					<li class="nav-button"><a href="login.php">LOG IN</a></li>
+					<a href="cart.php" id="cart-button"><img src="images/cart.png" id="cart-image"/> CART</a>
+				</ul>
+			</nav>
+		</div>
+	</div>
+	<div id="grey-box">
+		<div ìd="white-box">
+			<img src="images/packet-s.jpg" />
+			<a href="shop.php" id="shop-now-button">SHOP NOW &rarr;</a>
+		</div>
+	</div>
+</body>
+</html>
