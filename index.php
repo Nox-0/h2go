796a22242a64dbb1c5eafd44671063e895aef53b
Nox-0 <dbublik11@gmail.com>
2018-05-04 15:50:26 +1200

    I did things and am angry because things don't work
    It's super hot in here and my brain isn't functioning bagguette

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8" />
-	<title>Home </title>
+	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="css/styles.css" />
 </head>
 <body>
@@ -11,19 +11,17 @@
 			<a href="index.php"><img src="images/logo.png"  id="logo-image"/></a>
 			<nav>
 				<ul id="nav-buttons">
-					<li class="nav-button"><a href="shop.php">SHOP</a></li>
-					<li class="nav-button"><a href="signup.php">SIGN UP</a></li>
-					<li class="nav-button"><a href="login.php">LOG IN</a></li>
-					<a href="cart.php" id="cart-button"><img src="images/cart.png" id="cart-image"/> CART</a>
+					<li class="nav-button"><strong><a href="shop.php">SHOP</a></li></strong>
+					<li class="nav-button"><strong><a href="signup.php">SIGN UP</a></li></strong>
+					<li class="nav-button"><strong><a href="login.php">LOG IN</a></li></strong>
+					<strong><a href="cart.php" id="cart-button"><img src="images/cart.png" id="cart-image"/> CART</a></strong>
 				</ul>
 			</nav>
 		</div>
 	</div>
-	<div id="grey-box">
-		<div ìd="white-box">
-			<img src="images/packet-s.jpg" />
-			<a href="shop.php" id="shop-now-button">SHOP NOW &rarr;</a>
-		</div>
+	<div class="white-box">
+		<img src="images/packet-s.jpg" />
+		<a href="shop.php" id="shop-now-button">SHOP NOW &rarr;</a>
 	</div>
 </body>
 </html>
