e23a5434e580bee723337e58ad889faa5a7c4ad4
Nox-0 <dbublik11@gmail.com>
2018-05-10 10:47:15 +1200

    Worked more on the home page
    I made the middle of the page fit properly and formatted everything in
    the middle of the to make it look how it is on the wireframe. I also
    fixed a bit of the nav bar to make the nav buttons properly spaced
    between each other, however, that's as far as I got with that. WIll work
    on it a bit later.
    I also cropped the packet-s.jpg image for easier manipulation on the
    webpage and added a favicon because I thought that it would be cool to
    have one.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8" />
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="css/styles.css" />
+	<link rel="icon" href="images/favicon.png" />
 </head>
 <body>
 	<div id="nav-bar-container">
@@ -14,14 +15,19 @@
 					<li class="nav-button"><strong><a href="shop.php">SHOP</a></li></strong>
 					<li class="nav-button"><strong><a href="signup.php">SIGN UP</a></li></strong>
 					<li class="nav-button"><strong><a href="login.php">LOG IN</a></li></strong>
-					<strong><a href="cart.php" id="cart-button"><img src="images/cart.png" id="cart-image"/> CART</a></strong>
 				</ul>
+				<div>
+					<strong><a href="cart.php" id="cart-button"><img src="images/cart.png" id="cart-image"/> CART</a></strong>
+				</div>
 			</nav>
 		</div>
 	</div>
-	<div class="white-box">
-		<img src="images/packet-s.jpg" />
-		<a href="shop.php" id="shop-now-button">SHOP NOW &rarr;</a>
+	<div id="white-box-holder">
+		<div id="white-box">
+			<img id="front-page-image" src="images/packet-s.jpg" />
+			<h1 id="image-header">SMALL PACKET OF WATER</h1>
+			<a href="shop.php" id="shop-now-button"><strong>SHOP NOW &rarr;</strong></a>
+		</div>
 	</div>
 </body>
 </html>
