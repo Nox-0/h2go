1070b84a924bf12ffeb5724123ff80134c49f258
Nox-0 <dbublik11@gmail.com>
2018-05-23 16:30:06 +1200

    Log out
    I had a logout function but it didn't work so I deleted it. It was:
    <a href="<?php unset($_SESSION["UserID"]);?>">LOG OUT</a> which logically should log you out if you press the log out button but instead it logs you out whenever you press any button which isn't what I wanted, hence me deleting it.

--- update.php
+++ update.php
@@ -32,8 +32,8 @@ include"conn.php";
 				<ul id="nav-buttons">
 					<li class="nav-button"><strong><a href="index.php">HOME</a></strong></li>
 					<li class="nav-button"><strong><a href="shop.php">SHOP</a></strong></li>
-					<li class="nav-button"><strong><a href="signup.php">SIGN UP</a></strong></li>
-					<li class="nav-button"><strong><a href="login.php">LOG IN</a></strong></li>
+					<li class="nav-button"><strong><a href="update.php">UPDATE</a></strong></li>
+					<li class="nav-button"><strong><a href="">LOG OUT</a></strong></li>
 				</ul>
 				<div>
 					<strong><a href="cart.php" id="cart-button"><img src="images/cart.png" id="cart-image"/> CART</a></strong>
